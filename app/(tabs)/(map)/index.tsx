1d05aa51032d22b24f556d6ce8d1bb1562f45d19
Hugo Dalmeida Vázquez <hugodalmeidavazquez99@gmail.com>
2025-05-04 15:38:39 +0200

    ADD - Basic map implementation
    * Basic device permissions handling
    * Show basic map with location button
    * Refresh current location when user moves

--- app/(tabs)/(map)/index.tsx
+++ app/(tabs)/(map)/index.tsx
@@ -1,66 +1,103 @@
 import { Camera, MapView, MarkerView } from "@maplibre/maplibre-react-native";
 import { useEffect, useState } from "react";
-import { PermissionsAndroid, Image, StyleSheet } from "react-native";
-import Geolocation from "@react-native-community/geolocation";
+import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
+import Geolocation, {
+  GeolocationError,
+  GeolocationResponse,
+} from "@react-native-community/geolocation";
+import { MaterialIcons } from "@expo/vector-icons";
+import { request, PERMISSIONS, RESULTS } from "react-native-permissions";
+import {
+  isLocationEnabled,
+  promptForEnableLocationIfNeeded,
+} from "react-native-android-location-enabler";
 
 export default function Map() {
   const [location, setLocation] = useState<any>({
     latitude: 55.6761,
     longitude: 12.5683,
   });
+  const [locationPermission, setLocationPermission] = useState(false);
+  const [locationEnabled, setLocationEnabled] = useState(false);
 
-  const requestLocationPermission = async () => {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: "Location Permission",
-        message: "This app needs access to your location.",
-        buttonNeutral: "Ask Me Later",
-        buttonNegative: "Cancel",
-        buttonPositive: "OK",
-      },
-    );
-
-    return granted === PermissionsAndroid.RESULTS.GRANTED;
+  const setCurrentLocation = (position: GeolocationResponse) => {
+    const { latitude, longitude } = position.coords;
+    setLocation({ latitude, longitude });
   };
 
-  // If we can access the current location, we will show the
-  // location the user setted as "home"
-  const fetchLocation = async () => {
-    const hasPermission = await requestLocationPermission();
+  const locationErrorCallback = (error: GeolocationError) => {
+    if (error.POSITION_UNAVAILABLE === error.code) setLocationEnabled(false);
+  };
 
-    if (hasPermission) {
-      Geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });
-        },
-        (error) => console.error("Error getting location:", error),
-        { enableHighAccuracy: false, timeout: 15000, maximumAge: 10000 },
-      );
+  const fetchLocation = async () => {
+    Geolocation.getCurrentPosition(setCurrentLocation, locationErrorCallback, {
+      enableHighAccuracy: false,
+      timeout: 15000,
+      maximumAge: 0,
+    });
+  };
+
+  const getCurrentLocationClick = async () => {
+    if (locationEnabled) fetchLocation();
+    else {
+      try {
+        await promptForEnableLocationIfNeeded();
+        setLocationEnabled(true);
+        fetchLocation();
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setLocationEnabled(false);
+        }
+      }
     }
   };
 
   useEffect(() => {
-    fetchLocation();
+    const onMounted = async () => {
+      const requestLocationResponse = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+      setLocationPermission(requestLocationResponse === RESULTS.GRANTED);
+      setLocationEnabled(await isLocationEnabled());
+
+      if (locationPermission && locationEnabled) fetchLocation();
+    };
+
+    const watchUserLocation = () => {
+      Geolocation.watchPosition(setCurrentLocation, locationErrorCallback, {
+        distanceFilter: 10,
+        enableHighAccuracy: false,
+      });
+    };
+
+    onMounted();
+    watchUserLocation();
   }, []);
 
   return (
-    <MapView
-      style={{ flex: 1 }}
-      mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=PMK5ywsC29zKMVfrB4U4"
-    >
-      <Camera
-        centerCoordinate={[location.longitude, location.latitude]}
-        zoomLevel={15}
-      />
-      <MarkerView coordinate={[location.longitude, location.latitude]}>
-        <Image
-          source={require("@/assets/icons/tractor.png")}
-          style={styles.tractorIcon}
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=PMK5ywsC29zKMVfrB4U4"
+      >
+        <Camera
+          centerCoordinate={[location.longitude, location.latitude]}
+          zoomLevel={15}
         />
-      </MarkerView>
-    </MapView>
+        <MarkerView coordinate={[location.longitude, location.latitude]}>
+          <Image
+            source={require("@/assets/icons/tractor.png")}
+            style={styles.tractorIcon}
+          />
+        </MarkerView>
+      </MapView>
+      <TouchableOpacity style={styles.button} onPress={getCurrentLocationClick}>
+        <MaterialIcons
+          name={locationEnabled ? "my-location" : "location-searching"}
+          size={24}
+        />
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -69,5 +106,14 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
     resizeMode: "contain",
+  },
+  button: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    backgroundColor: "#D3D3D3",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
   },
 });
